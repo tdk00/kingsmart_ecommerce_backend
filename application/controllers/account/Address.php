3e92526568227228b7fb0a2e607da13191aeedff
Nurlan Hamidzade <56442688+tdk00@users.noreply.github.com>
2022-06-09 03:36:04 +0400

    Account/address

---
+++ application/controllers/account/Address.php
@@ -0,0 +1,82 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+use chriskacerguis\RestServer\RestController;
+/**
+ * Class Address
+ * @property  address_model $AddressModel
+ * @property $form_validation
+ */
+class Address extends RestController
+{
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('account/address_model', "AddressModel");
+		$this->load->library('form_validation');
+	}
+
+	public function fetch_addresses_post()
+	{
+		sleep(2);
+		$userId = $this->post('user_id');
+
+		$addresses = $this->AddressModel->getAddresses( (int)$userId );
+		if( count( $addresses ) == 0 )
+		{
+			$this->response( [ "status" => FALSE, "data" => [] ] , 200 );
+		}
+		$this->response( [ "status" => TRUE, "data" => $addresses ] , 200 );
+	}
+
+	public function fetch_address_by_id_post()
+	{
+		$userId = $this->post('user_id');
+		$addressId = $this->post('address_id');
+
+		$address = $this->AddressModel->getAddressById( (int)$userId, (int)$addressId );
+		if( count( $address ) == 0 )
+		{
+			$this->response( [ "status" => FALSE, "data" => [] ] , 200 );
+		}
+		$this->response( [ "status" => TRUE, "data" => $address[0] ] , 200 );
+	}
+
+	public function add_or_edit_address_post()
+	{
+//		sleep(2);
+		$userId = $this->post('user_id');
+		$addressDetails = $this->post('address_details');
+
+		if( $addressDetails['id'] > 0 )
+		{
+			$this->AddressModel->updateAddress( (int)$userId, $addressDetails );
+			$this->response( [ "status" => TRUE ] , 200 );
+		}
+		elseif( $addressDetails['id'] == 0 )
+		{
+			$this->AddressModel->insertAddress( (int)$userId, $addressDetails );
+			$this->response( [ "status" => TRUE ] , 200 );
+		}
+
+	}
+
+	public function delete_address_post()
+	{
+//		sleep(2);
+		$userId = $this->post('user_id');
+		$addressId = $this->post('address_id');
+
+		if( $addressId > 0 )
+		{
+			$this->AddressModel->deleteAddress( (int)$userId, $addressId );
+			$this->response( [ "status" => TRUE ] , 200 );
+		}
+		else
+		{
+			$this->response( [ "status" => false ] );
+		}
+
+	}
+}
---
+++ application/models/account/Address_model.php
@@ -0,0 +1,73 @@
+<?php
+class Address_model extends CI_Model
+{
+	public function __construct()
+	{
+		$this->load->database();
+	}
+
+	public function getAddresses( $userId = 0 )
+	{
+		$this->db->select("*");
+		$this->db->from("address");
+		$this->db->where("userId", $userId);
+		$query = $this->db->get();
+		return $query->result_array();
+	}
+
+	public function getAddressById( $userId = 0, $addressId = 0 )
+	{
+		$this->db->select("*");
+		$this->db->from("address");
+		$this->db->where("userId", $userId);
+		$this->db->where("id", $addressId);
+		$query = $this->db->get();
+		return $query->result_array();
+	}
+
+	public function updateAddress( $userId , $addressDetails  )
+	{
+		$this->resetDefaultAddresses($userId);
+
+		$this->db->where('userId', $userId);
+		$this->db->where('id', $addressDetails['id']);
+		unset($addressDetails['id']);
+		$this->db->update('address', $addressDetails);
+		$this->db->trans_complete();
+		if ($this->db->trans_status() === FALSE)
+		{
+			return false;
+		}
+		return true;
+	}
+	public function insertAddress( $userId, $addressDetails )
+	{
+		$this->resetDefaultAddresses($userId);
+		unset($addressDetails['id']);
+		$addressDetails['selected'] = 1;
+		$addressDetails['userId'] = $userId;
+		$this->db->insert('address', $addressDetails );
+		return $this->db->insert_id();
+	}
+
+	public function resetDefaultAddresses( $userId = 0 )
+	{
+		$this->db->where('userId', $userId);
+		$this->db->update('address', [ 'selected' => 0 ]);
+		$this->db->trans_complete();
+		if ($this->db->trans_status() === FALSE)
+		{
+			return false;
+		}
+		return true;
+	}
+
+	public function deleteAddress( $userId = 0, $addressId )
+	{
+		$this->db->where('id', $addressId);
+		$this->db->where('userId', $userId);
+		$this->db->delete('address');
+		return true;
+	}
+
+}
